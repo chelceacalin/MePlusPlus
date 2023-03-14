49b82b8fc8f25032aa74ec309ce9f848723470ba
Calin <chelceacalin1@gmail.com>
2023-03-14 22:43:12 +0200

    Added Step Counter

--- app/src/main/java/com/example/meplusplus/WaterReminder/DrinkWater.java
+++ app/src/main/java/com/example/meplusplus/WaterReminder/DrinkWater.java
@@ -1,20 +1,31 @@
 package com.example.meplusplus.WaterReminder;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import com.example.meplusplus.MainActivity;
 import com.example.meplusplus.R;
@@ -22,7 +33,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class DrinkWater extends AppCompatActivity {
+public class DrinkWater extends AppCompatActivity implements SensorEventListener {
 
     //Controale
     SeekBar activity_drink_water_seekbar;
@@ -45,10 +56,35 @@ public class DrinkWater extends AppCompatActivity {
     SharedPreferences prefs;
     SharedPreferences.Editor editor;
 
+
+
+
+    //  Steps counter
+    private TextView stepCountTextView;
+    private TextView calories_burned;
+    private SensorManager sensorManager;
+    private Sensor stepSensor;
+    private int stepCount = 0;
+    private boolean isSensorRegistered = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink_water);
+
+        // Get reference to the UI components
+        stepCountTextView = findViewById(R.id.step_count);
+        calories_burned=findViewById(R.id.calories_burned);
+
+        // Get reference to the SensorManager
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        // Get reference to the step sensor if available
+        stepSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if (stepSensor == null) {
+            Toast.makeText(this, "Step sensor not available", Toast.LENGTH_SHORT).show();
+        }
         init();
         prefs = getSharedPreferences("wantReminders", MODE_PRIVATE);
         boolean wantReminders = prefs.getBoolean("yesToReminders", false);
@@ -147,11 +183,57 @@ public class DrinkWater extends AppCompatActivity {
         reference = database.getReference();
     }
 
+
     @Override
     public void onBackPressed() {
         startActivity(new Intent(DrinkWater.this, MainActivity.class));
         overridePendingTransition(R.anim.slide_left_to_right_transition, R.anim.slide_right_to_left_transition);
         finish();
         super.onBackPressed();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Register the step sensor listener
+        if (stepSensor != null && !isSensorRegistered) {
+            sensorManager.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            isSensorRegistered = true;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Unregister the step sensor listener
+        if (stepSensor != null && isSensorRegistered) {
+            sensorManager.unregisterListener(this, stepSensor);
+            isSensorRegistered = false;
+        }
+    }
+int i=1;
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
+            // Update the step count
+            stepCount = (int) event.values[0];
+            stepCountTextView.setText(String.valueOf(stepCount));
+            int caloriesB=(int) ((double)stepCount*0.063);
+            calories_burned.setText(caloriesB+"");
+
+            if(stepCount<1000)
+            stepCountTextView.setTextColor(Color.BLUE);
+
+            if(stepCount>8000){
+                stepCountTextView.setTextColor(Color.GREEN);
+            }
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do nothing
     }
 }
